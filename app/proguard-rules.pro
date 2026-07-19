f1a0636443527c694c3ff6eaaec11cf96bc2df86
🐾白墨🐾Whiteink🐾 <111621443+Whiteink666@users.noreply.github.com>
2026-07-19 17:48:57 +0800

    Delete app directory

--- app/proguard-rules.pro
+++
@@ -1,3 +0,0 @@
-# Add project specific ProGuard rules here.
--keepattributes SourceFile,LineNumberTable
--keepclassmembers class com.whiteink.coloros.** { *; }
--- app/src/main/java/com/whiteink/coloros/FluidCloudApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.whiteink.coloros
-
-import android.app.Application
-
-class FluidCloudApplication : Application() {
-    // 通知渠道在 FluidCloudService 中动态创建，
-    // 不需在此初始化，参考项目也是这么做的
-}
